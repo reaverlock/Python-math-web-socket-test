4702c6848ddf56e6c02e317ba386297bbdc491ce
Pedro Baumann <sinmail@gmail.com>
2014-02-07 01:25:43 -0500

    probando formulas matematicas, aun no logromanejar varios websockets distintos

--- assets/js/WebSocketTestHandler.js
+++ assets/js/WebSocketTestHandler.js
@@ -1,14 +1,14 @@
 // assets/js/WebSocketTestHandler.js
 
 /**
- *  This event handler responds to the WebSocketClient, customizing the actions we take 
+ *  This event handler responds to the WebSocketClient, customizing the actions we take
  *  when it issues events such as onOpen or onMessage.
  *
  *  We implement this separately so that we can perform layout-specific changes while
  *  keeping the WebSocketClient abstracted from UI
  */
 function WebSocketTestHandler(console) {
-	this.con = console;
+    this.con = console;
 }
 
 /**
@@ -16,41 +16,42 @@ function WebSocketTestHandler(console) {
  *  publish your message into the console
  */
 WebSocketTestHandler.prototype.send = function(message) {
-	this.con.appendFromClient(message);
+    this.con.appendFromClient(message);
 }
 
 /**
- * upon connection, 
- *   - exchange the Connect button for a Disconnect button, 
+ * upon connection,
+ *   - exchange the Connect button for a Disconnect button,
  *   - allow user to send messages to the server,
  *   - notify user of the connection
  */
 WebSocketTestHandler.prototype.onOpen = function() {
-	$("#connected").html('<i class="icon-check"></i>');
-	$("#connect").hide();
-	$("#disconnect").show();
-	$("#send").removeClass("disabled");
- };
+    $("#connected").html('<i class="icon-check"></i>');
+    $("#connect").hide();
+    $("#disconnect").show();
+    $("#send").removeClass("disabled");
+    $("#suma").removeClass("disabled");
+};
 /**
  *  when a message is recieved from the server,
  *  publish the server's message into the console
  */
 WebSocketTestHandler.prototype.onMessage = function(e) {
-	var message = e.data;
-	this.con.appendFromServer(message);
+    var message = e.data;
+    this.con.appendFromServer(message);
 };
 
 /**
- * upon connection, 
- *   - exchange the Connect button for a Disconnect button, 
+ * upon connection,
+ *   - exchange the Connect button for a Disconnect button,
  *   - allow user to send messages to the server,
  *   - notify user of the connection
  */
 WebSocketTestHandler.prototype.onClose = function() {
-	$("#connected").html('<i class="icon-check-empty"></i>');
-	$("#disconnect").hide();
-	$("#connect").show();
-	$("#send").addClass("disabled");
+    $("#connected").html('<i class="icon-check-empty"></i>');
+    $("#disconnect").hide();
+    $("#connect").show();
+    $("#send").addClass("disabled");
 };
 
 /**
@@ -58,6 +59,6 @@ WebSocketTestHandler.prototype.onClose = function() {
  * print the error into the console
  */
 WebSocketTestHandler.prototype.onError = function(e) {
-	var message = e.data;
-	this.con.appendError(message);
+    var message = e.data;
+    this.con.appendError(message);
 };
--- echoserver.py
+++ echoserver.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python
- 
+
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
 
- 
+
+class Sumatoria(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        print "sumatoria connected"
+        self.write_message("sumatoria conected")
+
+    def on_message(self, message):
+        sumatoria = int(message) + int(message)
+        self.write_message(sumatoria)
+
+    def on_close(self):
+        print "Client disconnected sumatoria"
+
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-	def open(self):
-		print "New client connected"
-		self.write_message("You are connected")
 
-	def on_message(self, message):
-		self.write_message(message)
+    def open(self):
+        print "New client connected"
+        self.write_message("You are connected")
 
-	def on_close(self):
-		print "Client disconnected"
+    def on_message(self, message):
+        message = 'el cuadrado de lo que hiciiste es: %s' % str(
+            int(message) * int(message))
+        self.write_message(message)
+
+    def on_close(self):
+        print "Client disconnected"
 
 
 application = tornado.web.Application([
     (r"/", WebSocketHandler),
+    (r"/sumatoria", Sumatoria),
 ])
- 
+
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
-
-
-
